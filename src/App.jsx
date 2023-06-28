6f405a5ee20fadc0f167c4c2691974476d1d6565
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-06-28 02:52:09 -0300

    28/06/2023
    -Se implementa contextProvider
    -Se crea carpeta hooks con el hooks de useClima

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import AppClima from "./components/AppClima"
-import "./index.css"
+import {ClimaProvider} from "./context/ClimaProvider.jsx"
 
 
 function App() {
 
   return (
-    <>
+    <ClimaProvider>
+      <header>
+        <h1>Busca el clima</h1>
+      </header>
       <AppClima/>
-    </>
+    </ClimaProvider>
+      
+  
   )
 }
 
